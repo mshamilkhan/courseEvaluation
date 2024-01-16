8d3f2865e47f5177ca610d1ae55200381d7328a1
mskeditors <mskeditors@gmail.com>
2024-01-16 21:18:59 +0500

    is Numeric in minor updated

--- databaseLayer/minorDb.java
+++ databaseLayer/minorDb.java
@@ -31,9 +31,9 @@ public class minorDb {
             String insertQuery = "INSERT INTO minor_instrument (minor_name, minor_marks, minor_weight) VALUES (?, ?, ?)";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-                preparedStatement.setString(1, minorParams[0]); // minor_name
-                preparedStatement.setString(2, minorParams[1]); // minor_marks
-                preparedStatement.setString(3, minorParams[2]); // minor_weight
+                preparedStatement.setString(1, minorParams[0]); 
+                preparedStatement.setString(2, minorParams[1]); 
+                preparedStatement.setString(3, minorParams[2]); 
 
                 int rowsAffected = preparedStatement.executeUpdate();
 
@@ -48,7 +48,6 @@ public class minorDb {
             // String userName = resultSet.getString("username");
             // // String Password = resultSet.getString("password");
             // // ... retrieve other columns similarly
-
             // // Process the retrieved data (Here, simply printing)
             // System.out.println("Username: " + userName );
             // }
@@ -72,67 +71,3 @@ public class minorDb {
         }
     }
 }
-
-// // import domainLayer.minorValidation;
-
-// public class minorDb {
-// public static void minorDB(String minorNameText, String minorMarksText,String
-// minorWeightText) {
-// // Call the getMinorParamDb method and get the result
-// String[] minorParams =
-// dbController.getMinorParamDb(minorNameText,minorMarksText, minorWeightText);
-
-// // Access the minorNameText from the result
-
-// // Print the minorNameText
-// System.out.println("minorDb: --> " + minorParams[0]);
-
-// // JDBC URL for MySQL - ensure to replace 'your_database_name' with your
-// database name
-// String jdbcUrl = "jdbc:mysql://localhost:3306/courseEvaluationdb";
-// String username = "root";
-// String password = "QwertyQwerty11,";
-// Connection connection = null;
-// Statement statement = null;
-// ResultSet resultSet = null;
-// try {
-
-// // Establishing the connection
-// connection = DriverManager.getConnection(jdbcUrl, username, password);
-
-// // Creating a statement
-// statement = connection.createStatement();
-
-// // Executing a simple query
-// resultSet = statement.executeQuery("SELECT username FROM CE");
-// System.out.println("Database Connected");
-// // // Iterating through the result set
-// // while (resultSet.next()) {
-// // // Retrieve data by column name
-// // String userName = resultSet.getString("username");
-// // // String Password = resultSet.getString("password");
-// // // ... retrieve other columns similarly
-
-// // // Process the retrieved data (Here, simply printing)
-// // System.out.println("Username: " + userName );
-// // }
-// } catch (SQLException e) {
-// e.printStackTrace();
-// } finally {
-// // Closing the resources in a finally block to ensure they're always closed
-// try {
-// if (resultSet != null) {
-// resultSet.close();
-// }
-// if (statement != null) {
-// statement.close();
-// }
-// if (connection != null) {
-// connection.close();
-// }
-// } catch (SQLException e) {
-// e.printStackTrace();
-// }
-// }
-// }
-// }
--- domainLayer/minorValidation.java
+++ domainLayer/minorValidation.java
@@ -5,22 +5,45 @@ import interfaces.minor;
 
 
 
-public class minorValidation{
-    public static void minorinfoprocess(String minorNameText,String minorMarksText,String minorWeightText, String selectedMajorText )
-    {
+public class minorValidation {
+    public static void minorinfoprocess(String minorNameText, String minorMarksText, String minorWeightText, String selectedMajorText) {
+        // Create an instance of the 'minor' class to handle alerts/messages
         minor minoralert = new minor();
+
+        // Print the values of minor marks and minor name to the console
         System.out.println("Minor marks: " + minorMarksText);
         System.out.println("Minor Name: " + minorNameText);
-                if(minorMarksText.isEmpty() || selectedMajorText.isEmpty() || minorNameText.isEmpty() || minorWeightText.isEmpty()){
-                      minoralert.showError("Error: Some fields are null.");
-                }
-                else{
-                    
-                    minoralert.showSuccessMessage("This is a success message!");
-                    dbController.getMinorParamDb(minorNameText, minorMarksText, minorWeightText);
-                    minorDb.minorDB(minorNameText, minorMarksText, minorWeightText);
-                }
 
+        // Check if any of the required fields is empty
+        if (minorMarksText.isEmpty() || selectedMajorText.isEmpty() || minorNameText.isEmpty() || minorWeightText.isEmpty()) {
+            // Show an error message if any of the required fields is empty
+            minoralert.showError("Error: Some fields are null.");
+        } else {
+            // Check if both minor marks and weight are numeric
+            if (!isNumeric(minorMarksText) || !isNumeric(minorWeightText)) {
+                // Show an error message if either minor marks or weight is not numeric
+                minoralert.showError("Error: Minor marks and weight must be numeric values.");
+            } else {
+                // Show a success message if all checks pass
+                minoralert.showSuccessMessage("This is a success message!");
 
+                // Call the 'getMinorParamDb' method from the 'dbController' class
+                dbController.getMinorParamDb(minorNameText, minorMarksText, minorWeightText);
+
+                // Call the 'minorDB' method from the 'minorDb' class
+                minorDb.minorDB(minorNameText, minorMarksText, minorWeightText);
+            }
+        }
+    }
+
+    // Utility method to check if a string is numeric
+    private static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
+ 
